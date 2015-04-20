73b8b53c34b4c998d23dd653e590cdf41bd23df4
Adrian Ceballos <zzAdrianCeballos@gmail.com>
2015-04-20 12:17:24 -0700

    test

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Assessment</title>
+        <link rel="stylesheet" type="text/css" href="main.css"</link> 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+       
+        <script type="text/javascript" src="main.js"></script> 
+        
+    </head>
+    <body>
+        <div class = "dan">
+            <p>
+              dan  
+            </p>
+        </div>
+        <div id="iwao">
+            <p>
+               iwao 
+            </p>
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
